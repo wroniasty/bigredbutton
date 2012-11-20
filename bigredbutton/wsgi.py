75010c25d9d568cd273d5c22f7ea71c2818e5af4
wroniasty <wroniasty@gmail.com>
2012-11-20 14:28:11 +0100

    initial

--- bigredbutton/wsgi.py
+++ bigredbutton/wsgi.py
@@ -14,14 +14,34 @@ framework.
 
 """
 import os
+import sys
+import sys.path
+import site
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bigredbutton.settings")
+
+project_path = os.path.realpath ( os.path.dirname(__file__) + "/../" )
+vepath = os.path.realpath ( os.path.dirname(__file__) + "/../../" )
+vesite = os.path.realpath ( vepath + "/lib/python2.7/site-packages/" )
+
+prev_sys_path = list(sys.path)
+
+site.addsitedir(vesite)
+sys.path.append ( project_path )
+
+new_sys_path = [p for p in sys.path if p not in prev_sys_path]
+for item in new_sys_path:
+    sys.path.remove(item)
+sys.path[:0] = new_sys_path
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+import wsgi_monitor
+wsgi_monitor.start(interval=5.0)
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
